c6ed5984d43f22620fe7a8d9a034f42bbc610b65
oEbuka <ebukaobiora10@gmail.com>
2022-12-21 19:13:25 +0000

    lint correction on 1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-	body: 'Success',
+        body: 'Success',
       });
     }
     reject(new Error('The fake API is not working currently'));
